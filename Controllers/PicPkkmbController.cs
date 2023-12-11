71b743bea256ecf5299357f4959e77656cdef876
Muhammad Daffa Abiyyu Syaddad <daffaabiyyu04@gmail.com>
2023-12-11 13:19:55 +0700

    Interface Mahasiswa Baru

--- Controllers/PicPkkmbController.cs
+++ Controllers/PicPkkmbController.cs
@@ -21,19 +21,85 @@ namespace PKKMB_Interface.Controllers
 		{
 			Response.Cookies.Delete(".AspNetCore.Session");
 			ViewBag.role = "Pic";
-			return View("LoginPic");
+			return View();
 		}
 
 		[Route("picpkkmb/daftarpic")]
 		public IActionResult DaftarAkunPic()
 		{
-			return View("DaftarAkunPic");
+			return View();
 		}
 
 		[Route("picpkkmb/resetpasswordpic")]
 		public IActionResult LupaPasswordPic()
 		{
-			return View("LupaPasswordPic");
+			return View();
+		}
+
+		[Route("picpkkmb/panitiaksk")]
+		public IActionResult PanitiaKSK()
+		{
+			return View();
+		}
+
+		[Route("picpkkmb/jadwal")]
+		public IActionResult Jadwal()
+		{
+			return View();
+		}
+
+		[Route("picpkkmb/kelompok")]
+		public IActionResult Kelompok()
+		{
+			return View();
+		}
+
+		[Route("picpkkmb/ruangan")]
+		public IActionResult Ruangan()
+		{
+			return View();
+		}
+
+		[Route("picpkkmb/tugas")]
+		public IActionResult Tugas()
+		{
+			return View();
+		}
+
+		[Route("picpkkmb/absensi")]
+		public IActionResult Absensi()
+		{
+			return View();
+		}
+
+		[Route("picpkkmb/jamplus")]
+		public IActionResult JamPlus()
+		{
+			return View();
+		}
+
+		[Route("picpkkmb/jamminus")]
+		public IActionResult JamMinus()
+		{
+			return View();
+		}
+
+		[Route("picpkkmb/sikap")]
+		public IActionResult Sikap()
+		{
+			return View();
+		}
+
+		[Route("picpkkmb/kelulusan")]
+		public IActionResult Kelulusan()
+		{
+			return View();
+		}
+
+		[Route("picpkkmb/evaluasi")]
+		public IActionResult Evaluasi()
+		{
+			return View();
 		}
 	}
 }
