4104643cca0f47150a5507271745318207353c3a
Muhammad Daffa Abiyyu Syaddad <daffaabiyyu04@gmail.com>
2023-12-12 00:26:22 +0700

    Interface Ruangan (KSK)

--- Controllers/PanitiaKesekretariatanController.cs
+++ Controllers/PanitiaKesekretariatanController.cs
@@ -37,51 +37,87 @@ namespace PKKMB_Interface.Controllers
 		}
 
 		[Route("panitiakesekretariatan/informasimahasiswa")]
-        public IActionResult InformasiMahasiswa()
-        {
-            return View();
-        }
+		public IActionResult InformasiMahasiswa()
+		{
+			return View();
+		}
 
-        [Route("panitiakesekretariatan/informasijadwal")]
-        public IActionResult InformasiJadwal()
-        {
-            return View();
-        }
+		[Route("panitiakesekretariatan/informasijadwal")]
+		public IActionResult InformasiJadwal()
+		{
+			return View();
+		}
 
-        [Route("panitiakesekretariatan/informasikelompok")]
-        public IActionResult InformasiKelompok()
-        {
-            return View();
-        }
+		[Route("panitiakesekretariatan/informasikelompok")]
+		public IActionResult InformasiKelompok()
+		{
+			return View();
+		}
 
-        [Route("panitiakesekretariatan/informasiruangan")]
-        public IActionResult InformasiRuangan()
-        {
-            return View();
-        }
+		[Route("panitiakesekretariatan/informasiruangan")]
+		public IActionResult InformasiRuangan()
+		{
+			return View();
+		}
 
-        [Route("panitiakesekretariatan/informasitugas")]
-        public IActionResult InformasiTugas()
-        {
-            return View();
-        }
+		[Route("panitiakesekretariatan/informasitambahruangan")]
+		public IActionResult InformasiTambahRuangan()
+		{
+			return View();
+		}
 
-        [Route("panitiakesekretariatan/informasinilai")]
-        public IActionResult InformasiNilai()
-        {
-            return View();
-        }
+		[Route("panitiakesekretariatan/informasiubahruangan")]
+		public IActionResult InformasiUbahRuangan()
+		{
+			return View();
+		}
 
-        [Route("panitiakesekretariatan/informasievaluasi")]
-        public IActionResult InformasiEvaluasi()
-        {
-            return View();
-        }
+		[Route("panitiakesekretariatan/informasitugas")]
+		public IActionResult InformasiTugas()
+		{
+			return View();
+		}
 
-        [Route("panitiakesekretariatan/informasikelulusan")]
-        public IActionResult InformasiKelulusan()
-        {
-            return View();
-        }
-    }
+		[Route("panitiakesekretariatan/nilaitugas")]
+		public IActionResult NilaiTugas()
+		{
+			return View();
+		}
+
+		[Route("panitiakesekretariatan/nilaisikap")]
+		public IActionResult NilaiSikap()
+		{
+			return View();
+		}
+
+		[Route("panitiakesekretariatan/nilaijamplus")]
+		public IActionResult NilaiJamPlus()
+		{
+			return View();
+		}
+
+		[Route("panitiakesekretariatan/nilaijamminus")]
+		public IActionResult NilaiJamMinus()
+		{
+			return View();
+		}
+
+		[Route("panitiakesekretariatan/nilaiabsensi")]
+		public IActionResult NilaiAbsensi()
+		{
+			return View();
+		}
+
+		[Route("panitiakesekretariatan/evaluasi")]
+		public IActionResult Evaluasi()
+		{
+			return View();
+		}
+
+		[Route("panitiakesekretariatan/kelulusan")]
+		public IActionResult Kelulusan()
+		{
+			return View();
+		}
+	}
 }
