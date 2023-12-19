a0ab0e39dbb9d2fa57dedbf45fbef6493bfa45eb
Nabilabidah <abidahhh21@gmail.com>
2023-12-19 10:10:54 +0700

    Nilai,Jamplus,JamMinus

--- Controllers/SikapController.cs
+++ Controllers/SikapController.cs
@@ -2,11 +2,23 @@
 
 namespace PKKMB_Interface.Controllers
 {
-	public class SikapController : Controller
-	{
-		public IActionResult Index()
-		{
-			return View();
-		}
-	}
+    public class SikapController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult tambahnilaisikap()
+        {
+            return View();
+        }
+
+        [Route("sikap/ubahnilaisikap/{nls_idnilaisikap}")]
+        public IActionResult ubahnilaisikap(string nls_idnilaisikap)
+        {
+            ViewBag.nls_idnilaisikap = nls_idnilaisikap;
+            return View();
+        }
+    }
 }
